d60f07d89a0c83d1d044495339fa4769d14939d1
root <acs5698@rit.edu>
2018-11-16 13:52:57 -0500

    Fixed a few things

--- linfi/linfi.sh
+++ linfi/linfi.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Priya Gopani
+# Andrew Steiner 
+# main script to monitor files
 
 
 bak=$(auditctl -l)
@@ -7,19 +11,23 @@ echo $bak
 
 auditctl -D
 
-#files=(/boot/vimlinux/ /boot/vmlinuz /etc/bashrc /etc/crontab /etc/hosts /etc/hosts.allow /etc/host.deny /etc/group /etc/inittab /etc/passwd /etc/resolve.conf /proc/cpuinfo /proc/filesystems /etc/init.d /etc/profile.d /etc/rc.d/init.d /etc/shadow /tmp/)
+# Add your own files to monitor them!
+files=(/boot/vimlinux/ /boot/vmlinuz /etc/bashrc /etc/crontab /etc/hosts /etc/hosts.allow /etc/host.deny /etc/group /etc/inittab /etc/passwd /etc/resolve.conf /proc/cpuinfo /proc/filesystems /etc/init.d /etc/profile.d /etc/rc.d/init.d /etc/shadow /tmp/)
 
 
-files=(/etc/passwd)
+# creating rules to monitor files
 for i in "${files[@]}"; do
 	auditctl -w $i -k $i
 	echo $i
 done
 
 auditctl -l
-cat /etc/passwd &
+
 read -p "Press enter after you've installed/run the test program..."
+
 touch ausearchOut
+# Loop through all the rules, and search for recent events.
+# output csv, and to console
 for i in "${files[@]}"; do
 	
        	echo "**************************************************************************************"
@@ -34,8 +42,8 @@ for i in "${files[@]}"; do
 	#echo $res | aureport 
 	# Report about files and af_unix sockets	
 	#echo $res | aureport -f 
-
-	ausearch -k $i --start today --format csv >> ausearchOut
+	
+	ausearch -k $i --start recent --format csv >> ausearchOut
 
 	awk '!/NODE,EVENT,/' ausearchOut > temp && mv temp ausearchOut
 done
